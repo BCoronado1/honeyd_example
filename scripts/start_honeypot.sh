84378baffe742c957da14278fea42ebc5c8116c9
Braulio Coronado <bcoronado1@gmail.com>
2021-12-12 18:24:28 -0800

    Updates to add ethtool

--- scripts/start_honeypot.sh
+++ scripts/start_honeypot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+ethtool -K eth0 tx off
 honeyd -d -f honeypot.conf 192.168.56.101
 sleep 9999
--- scripts/start_nmap_scanner.sh
+++ scripts/start_nmap_scanner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+ethtool -K eth0 tx off
 echo "Waiting 2 seconds then scanning..."
 sleep 2
-nmap -sV 192.168.56.101 -p 22,80
+nmap -sV 192.168.56.101 -p 22
 sleep 9999
